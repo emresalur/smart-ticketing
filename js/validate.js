98fdf9feac21d1ece87c0ff57229cd57621cef99
qiuyf111 <72795199+qiuyf111@users.noreply.github.com>
2022-03-10 12:13:43 +0000

    new

--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,6 @@
 (function() {
 	/**
-	 * 所有类的基类，提供继承机制
+	 * Base class for all classes, providing inheritance mechanism 
 	 */
 	var initializing = false, fnTest = /xyz/.test(function() {xyz;}) ? /\b_super\b/ : /.*/;
 	this.Class = function() {};
@@ -93,33 +93,13 @@ var validate = Class
 			}
 		})
 var validateConstant = {
-	"notEmpty" : /^.+$/,// 合法字符
-	"password" : /^[0-9A-Za-z]{1,18}$/,// 密码
-	"rightfulString" : /^[A-Za-z0-9_-]+$/,// 合法字符
-	"number" : /^\d+$/,// 数字
-	"english" : /^[A-Za-z]+$/,// 纯英文
-	"numberEnglish" : /^[A-Za-z0-9]+$/,// 英文和数字
-	"float" : /^[+]?\d+(\.\d+)?$/,// 浮点型
-	"money" : /(^[1-9]\d{0,9}(\.\d{1,2})?$)/,
-	"chinese" : "/^[\u4e00-\u9fa5]+$/",// 纯中文
-	"mobile" : /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1})|(14[0-9]{1}))+\d{8})$/,// 手机号
-	"tel" : /^(\d{3,4}-?)?\d{7,9}$/g,// 电话
-	"qq" : /^[1-9]\d{4,12}$/,// qq
-	"email" : /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/,// 邮箱
-	"positive":/^[1-9][0-9]+$/,//大于0的数字
-
-	}
+	"notEmpty" : /^.+$/,
+	"password" : /^[0-9A-Za-z]{1,18}$/,
+	"email" : /^([a-zA-Z0-9_-])+@kcl.ac.uk+/,
 }
 var errorMsg = {
-	"notEmpty" : "This can not be empty.",
-	"password" : "Please enter your password",
-	"rightfulString" : "请输入合法字符",// 合法字符
-	"number" : "Number only",// 数字
-	"english" : "English only",// 纯英文
-	"numberEnglish" : "English and number only",// 英文和数字
-	"float" : "只能输入小数",// 浮点型
-	"mobile" : "Please enter your phone number",// 手机号
-	"zipCode" : "Please enter your zipcode",
-	"email" : "Please enter your email address",// 邮箱
-	
+	"notEmpty" : "Can not be empty ",
+	"password" : "Please enter the correct password ",
+	"rightfulString" : "Please enter legal characters ",
+	"email" : "please enter your vaild email ",
 }
