ba361af0440d94a5eab759a4dba4f44eea0563d8
LilDereky <derrickdeng00@gmail.com>
2022-04-01 16:28:06 +0100

    text fixed 2.0

--- ticketing/accounts/admin.py
+++ ticketing/accounts/admin.py
@@ -7,7 +7,7 @@ from django.contrib.auth.hashers import make_password
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', "is_teacher", "have_active")
     fieldsets = (
-        ['用户信息', {
+        ['username', {
             'fields': ('username', 'mpassword', "is_teacher","have_active"),
         }],
 
