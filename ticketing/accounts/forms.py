eebccc4dbce77ef7c5a05f73fc371eea824f856a
mateiaandreea <92314568+mateiaandreea@users.noreply.github.com>
2022-04-04 22:13:49 +0100

    Update forms.py
    fixed English

--- ticketing/accounts/forms.py
+++ ticketing/accounts/forms.py
@@ -5,65 +5,65 @@ import re
 import sys
 import os
 
-#登录表单
+#login form
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "用户名", "required": "required","id":"user","name":"username"}),
-                              max_length=100,error_messages={"required": "username不能为空",})
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "密码", "required": "required","id":"password","name":"password"}),
-                              max_length=200,error_messages={"required": "password不能为空",})
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "username", "required": "required","id":"user","name":"username"}),
+                              max_length=100,error_messages={"required": "username:Can not be empty",})
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "password", "required": "required","id":"password","name":"password"}),
+                              max_length=200,error_messages={"required": "password:Can not be empty",})
 
-#注册表单
+#registration form
 class RegForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "用户名", "required": "required",}),
-                              max_length=50,error_messages={"required": "用户名不能为空",})
-    email = forms.EmailField(widget=forms.TextInput(attrs={"placeholder": "邮箱", "required": "required",}),
-                              max_length=50,error_messages={"required": "邮件不能为空",})
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "密码", "required": "required",}),
-                              max_length=20,error_messages={"required": "密码不能为空",})
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "确认密码", "required": "required",}),
-                              max_length=20,error_messages={"required": "确认密码不能为空",})
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "username", "required": "required",}),
+                              max_length=50,error_messages={"required": "Username can not be empty",})
+    email = forms.EmailField(widget=forms.TextInput(attrs={"placeholder": "email", "required": "required",}),
+                              max_length=50,error_messages={"required": "Email cannot be empty",})
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "password", "required": "required",}),
+                              max_length=20,error_messages={"required": "password can not be blank",})
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "confirm password", "required": "required",}),
+                              max_length=20,error_messages={"required": "password can not be empty",})
 
     def clean(self):
         if not self.is_valid():
-             raise forms.ValidationError('所有项都为必填项')
+             raise forms.ValidationError('All fields are required')
         elif self.cleaned_data['confirm_password'] != self.cleaned_data['password']:
-            raise forms.ValidationError('两次输入密码不一致')
+            raise forms.ValidationError('The passwords entered twice does not match')
         else:
             cleaned_data = super(RegForm,self).clean()
         username = self.cleaned_data['username']
         is_email_exist = UserProfile.objects.filter(email=username).exists()
         is_username_exist = UserProfile.objects.filter(username=username).exists()
         if is_username_exist or is_email_exist:
-            raise forms.ValidationError(u"该账号已被注册")
+            raise forms.ValidationError(u"The account has already been registered")
 
         return cleaned_data
 
-#注册表单
+#registration form
 class RegPhoneForm(forms.Form):
-    # username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "用户名", "required": "required",}),
-    #                           max_length=50,error_messages={"required": "用户名不能为空",})
-    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "手机", "required": "required",}),
-                              max_length=50,error_messages={"required": "手机不能为空",})
-    # email = forms.EmailField(widget=forms.TextInput(attrs={"placeholder": "邮箱", }),
+    # username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "username", "required": "required",}),
+    #                           max_length=50,error_messages={"required": "Username can not be empty",})
+    phone = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "mobile phone", "required": "required",}),
+                              max_length=50,error_messages={"required": "Phone cannot be empty",})
+    # email = forms.EmailField(widget=forms.TextInput(attrs={"placeholder": "Mail", }),
     #                          max_length=50, )
-    # password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "密码", "required": "required",}),
-    #                           max_length=20,error_messages={"required": "密码不能为空",})
-    # confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "确认密码", "required": "required",}),
-    #                           max_length=20,error_messages={"required": "确认密码不能为空",})
+    # password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "password", "required": "required",}),
+    #                           max_length=20,error_messages={"required": "password can not be blank",})
+    # confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm password", "required": "required",}),
+    #                           max_length=20,error_messages={"required": "Confirmed password can not be blank",})
 
     def clean(self):
         if not self.is_valid():
-             raise forms.ValidationError('所有项都为必填项')
+             raise forms.ValidationError('All fields are required')
         else:
             cleaned_data = super(RegPhoneForm,self).clean()
         phone = self.cleaned_data['phone']
         is_mobile_exist = UserProfile.objects.filter(mobile=phone).exists()
         if is_mobile_exist:
-            raise forms.ValidationError(u"该手机号已被注册")
+            raise forms.ValidationError(u"The phone number has already been registered")
         p2 = r'^0\d{2,3}\d{7,8}$|^1[3578]\d{9}$|^147\d{8}'
         phonematch = re.findall(r'^0\d{2,3}\d{7,8}$|^1[358]\d{9}$|^147\d{8}', phone)
         if len(phonematch) == 0 or phonematch==None or phonematch=="" :
-            raise forms.ValidationError(u"该手机号格式错误")
+            raise forms.ValidationError(u"The phone number is in the wrong format")
         return cleaned_data
 
 
