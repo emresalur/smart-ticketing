3632ba257139919912ec6f169d6c3df3044e45fe
LilDereky <47183092+LilDereky@users.noreply.github.com>
2022-04-05 11:33:08 +0100

    Update tests.py

--- ticketing/accounts/tests.py
+++ ticketing/accounts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
-
+# Zejin.Deng
 # Create your tests here.
+# creat test class
+from accounts import models
+
+
+class AccoutsTest(TestCase):
+
+    def setUp(self):
+        '''
+        Equivalent to the initialization operation, it will be executed once before the test case test_xxx is executed
+        Features: This function will be executed once before each test case is executed
+        '''
+        print('start testing Accouts part!!!')
+
+
+    def tearDown(self):
+        '''
+        Will be executed once after the test case test_xxx is executed
+         Features: It will be executed once after each test case is executed
+        '''
+        print('>>>>Test end Accouts part!!! Test passed')
+
+
+    # Define a test function The naming format of the test function is test+function name
+    def test_accoutsTest(self):
+        print('Generating UserProfile entity')
+        models.UserProfile.objects.create(mpassword='123',email="123",is_active=False,have_active=False)
+        print("UserProfile entity created successfully")
+        print("Querying the test database for test data")
+        print(models.UserProfile.objects.all().values())
