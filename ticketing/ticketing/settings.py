659f203a3d2bd60aa44b6cbb561edc4c37601e8d
mateiaandreea <92314568+mateiaandreea@users.noreply.github.com>
2022-04-29 15:10:37 +0100

    Update settings.py

--- ticketing/ticketing/settings.py
+++ ticketing/ticketing/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'ticketing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ciba',
   # test database
         'TEST': {
             'CHARSET':'utf8',
